27df34c6d72a24c388e8321e22cf22e3356043dc
rodshokrian <rodshokrian@gmail.com>
2017-08-05 16:23:24 -0700

    srlToRegex available on window

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+/* Testing purposes only */
+import { srlToRegex } from '../imports/api/translator';
+window.srlToRegex = srlToRegex;
+/* --------------------- */
+
 import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
