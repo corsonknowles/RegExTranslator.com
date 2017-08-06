83ba47b7ec9271f8bda44ccefdc76dd94d209948
boothandrewd <boothandrewd@gmail.com>
2017-08-05 17:12:26 -0700

    Run some SRL on the frontend

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from '../imports/ui/App.jsx';
+import App from '../imports/components/App.jsx';
+import configureStore from '../imports/store/store';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('root'));
+  const store = configureStore();
+
+  render(<App store={store} />, document.getElementById('root'));
 });
---
+++ imports/components/Translator.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SrlInput from './translator/srl_input';
+
+export default () => (
+  <div className="translator">
+    <SrlInput />
+  </div>
+);
