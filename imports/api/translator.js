ab3dccfda119db7ddcb94d3e586c2f856172c929
adam <amartenj@gmail.com>
2017-08-07 09:58:55 -0700

    Add skeleton of regexToSrl translation method

--- imports/api/translator.js
+++ imports/api/translator.js
@@ -4,3 +4,14 @@ export const srlToRegex = (srl) => {
   const query = new SRL(srl);
   return query.get().toString();
 };
+
+export const regexToSrl = regex => {
+
+  // console.log("Start. Create root node.");
+  let depth = 0;
+  for (let i = 0; i < regex.length; i++) {
+
+  }
+
+  return "Translation coming soon";
+};
