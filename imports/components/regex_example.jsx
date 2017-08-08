a96f9f07d5fb574133735073effba6f5166a8439
boothandrewd <boothandrewd@gmail.com>
2017-08-07 19:36:19 -0700

    Re-implement highlighting to use consecutive indices

--- imports/components/regex_example.jsx
+++ imports/components/regex_example.jsx
@@ -19,37 +19,44 @@ export default connect(mapStateToProps, mapDispatchToProps)(
       this.handleExampleInputChange = this.handleExampleInputChange.bind(this);
     }
 
-
     componentDidUpdate() {
       const { props: { regexText }, state: { exampleText } } = this;
 
+      const regex = new RegExp(regexText, 'g');   // NOTE: Global flag set
       const matches = {};
-      const regex = new RegExp(regexText, 'g');
-      let match, counter = 1;
+      const matchIndices = new Set;
+      let match, counter = 0;
       while ((match = regex.exec(exampleText)) !== null) {
-        if (counter >= 1000) break;   // To avoid infinite match loops
+        if (counter >= 100) break;   // Avoid infinite match loops
+
         matches[match.index] = match;
+
+        for (let i = 0; i < match[0].length; i++) {
+          matchIndices.add(match.index + i);
+        }
+
         counter++;
       }
 
-      // TODO: Improve highlighting markup generation algorithm
       let resultsText = '';
+      let currentMatch;
       for (let idx = 0; idx < exampleText.length; idx++) {
-        if (matches[idx]) {
-          resultsText += `<span>${matches[idx][0]}</span>`;
-          idx += matches[idx][0].length - 1;
-        } else {
-          resultsText += exampleText[idx];
+        if (!matchIndices.has(idx - 1) && matchIndices.has(idx)) {
+          resultsText += '<span>';
         }
+
+        if (matchIndices.has(idx - 1) && !matchIndices.has(idx)) {
+          resultsText += '</span>';
+        }
+
+        resultsText += exampleText[idx];
       }
 
       this.resultsBox.innerHTML = resultsText;
     }
 
     handleExampleInputChange(event) {
-      this.setState({
-        exampleText: event.target.value,
-      });
+      this.setState({ exampleText: event.target.value });
     }
 
     render() {
@@ -60,13 +67,9 @@ export default connect(mapStateToProps, mapDispatchToProps)(
             value={this.state.exampleText}
           />
 
-          |
-          <br/>
-          v
+          |<br/>v
 
-          <div
-            ref={el => { this.resultsBox = el; }}
-          />
+          <div ref={el => { this.resultsBox = el; }} />
         </div>
       );
     }
