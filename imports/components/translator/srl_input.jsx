ec334a338e376ea4153b75b450e119d8d722db19
boothandrewd <boothandrewd@gmail.com>
2017-08-07 20:23:30 -0700

    Comment major logic sections

--- imports/components/translator/srl_input.jsx
+++ imports/components/translator/srl_input.jsx
@@ -36,14 +36,19 @@ export default connect(mapStateToProps, mapDispatchToProps)(
     }
 
     srlInputHandler(event) {
+      // Set SRL slice
       this.props.receiveSrl(event.target.value);
+
       try {
+        // NOTE: Error causing line
         const regexText = new Srl(event.target.value).getRawRegex();
 
+        // Set regex to SRL-translated version and clear errors
         this.props.setRegex(regexText);
         this.props.clearSrlInputErrors();
         this.srlInputBox.style.border = null;
       } catch(error) {
+        // If SRL parsing fails, set errors
         this.props.receiveSrlErrors(['Invalid SRL syntax', error]);
         this.srlInputBox.style.border = '1px solid Red';
       }
