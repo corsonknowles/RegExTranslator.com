472f4a31a519ab355812f8babccfe9d6d1748617
rodshokrian <rodshokrian@gmail.com>
2017-08-09 11:39:37 -0700

    add preset patterns box style patterns box fix DB requests and redux cycle for patterns

--- imports/components/translator/regex_input.jsx
+++ imports/components/translator/regex_input.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { receiveRegex, getRegexs, createRegex } from '../../actions/regex_actions';
 import { bindActionCreators } from 'redux';
+import PatternDropdown from './pattern_dropdown.jsx';
 
 const mapStateToProps = (state) => ({
   regexText: state.regex.regexText,
@@ -24,6 +25,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(
       };
 
       this.regexInputHandler = this.regexInputHandler.bind(this);
+      this.regexSelector = this.regexSelector.bind(this);
     }
 
     componentDidMount() {
@@ -39,15 +41,32 @@ export default connect(mapStateToProps, mapDispatchToProps)(
       // TODO: Run reverse translation here
     }
 
+    regexSelector(pattern) {
+      this.setState({ regexInputText: pattern });
+    }
+
     render() {
-      debugger;
+      //Initialize a variable to hold our PatternDropdown component (if
+      // we've received our regexs)
+      let DropdownComponent;
+      if (Object.keys(this.props.regexs).length > 0) {
+        DropdownComponent = <PatternDropdown
+                            regexs={this.props.regexs}
+                            regexSelector={this.regexSelector} />;
+      } else {
+        DropdownComponent = <div />;
+      }
+
       return (
-        <div className="translator-input-section">
-          <h2>Regular Expression</h2>
-          <textarea
-            onChange={this.regexInputHandler}
-            value={this.state.regexInputText}
-          />
+        <div className="translator-container">
+          <div className="translator-input-section">
+            <h2>Regular Expression</h2>
+            <textarea
+              onChange={this.regexInputHandler}
+              value={this.state.regexInputText}
+            />
+          </div>
+          {DropdownComponent}
         </div>
       );
     }
