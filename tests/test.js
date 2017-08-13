74a7cfa44cb41d6a0185e4636889389af6daf35d
adam <amjacobson0@gmail.com>
2017-08-13 13:44:35 -0700

    Add unit tests with Mocha

---
+++ tests/test.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+
+import { srlToRegex, regexToSrl } from '../imports/api/translator';
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+describe('translation', function() {
+  describe('charset', function() {
+    it('should translate common charsets', function() {
+      assert.equal(regexToSrl("[a-z]"), "letter");
+      assert.equal(regexToSrl("[A-Z]"), "uppercase");
+      assert.equal(regexToSrl("[0-9]"), "digit");
+    });
+
+    it('should translate custom ranges', function() {
+      assert.equal(regexToSrl("[d-g]"), "letter from d to g");
+      assert.equal(regexToSrl("[S-X]"), "uppercase letter from S to X");
+      assert.equal(regexToSrl("[3-6]"), "digit from 3 to 6");
+    });
+
+    it('should translate combined ranges', function() {
+      assert.equal(regexToSrl("[a-zA-Z0-9%#_]"), "any of (letter, uppercase, digit, one of \"%#_\")");
+    });    
+  });
+});
+
+const stringFromRegex = regex => {
+
+};
