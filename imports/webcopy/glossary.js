509385bce90853670dd2c1ee0fc469f6ed00c248
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-08-12 22:12:43 -0700

    Add glossary and help text

---
+++ imports/webcopy/glossary.js
@@ -0,0 +1,42 @@
+export default
+
+`Glossary:
+'any character' \\w
+'any of (a, b, c)' (a|b|c)
+'anything' .
+'at least 8 times' {8,}
+'backslash' \\
+'between 3 and 7 times' {3,7}
+'capture a' (a)
+'case insensitive' /myRegex/i
+'digit' \\d
+'digit from 3 to 5' [3-5]
+'either of (x, y, z)' (x|y|z)
+'exactly once' {1}
+'exactly twice' {2}
+'exactly 4 times' {4}
+'if followed by' -> capture (digit) if followed by (any character) -> /([0-9])(?=\\w)/
+'if not followed by' -> capture (letter) if not followed by (digit) /([a-z])(?![0-9])/
+'letter' [a-z]
+'letter from g to m' [g-m]
+'literally "a string to match"' -> a string to match
+'multi line' /yourRegex/m
+'must end' $
+'never or more' *
+'new line' \\n
+'no character' \\W
+'no whitespace' \\S
+'number from' -> number from 4 to 6 -> [4-6]
+'once' {1}
+'one of "defg1234"' [defg1234]
+'optional' ?
+'once or more' +
+'raw "[a-zA-Z]"' -> [a-zA-Z]
+'starts with' ^
+'tab' \\t
+'twice',
+'until' -> capture (anything once or more) until "." -> (.+?)(?:\.)
+'uppercase letter' [A-Z]
+'uppercase letter from M to P' [M-P]
+'whitespace' \\s
+`;
--- imports/webcopy/help_text.js
+++ imports/webcopy/help_text.js
@@ -1,45 +1,6 @@
 export default
 
-`
-'any character' \\w
-'any of (a, b, c)' (a|b|c)
-'anything' .
-'at least 8 times' {8,}
-'backslash' \\
-'between 3 and 7 times' {3,7}
-'capture a' (a)
-'case insensitive' /myRegex/i
-'digit' \\d
-'digit from 3 to 5' [3-5]
-'either of (x, y, z)' (x|y|z)
-'exactly once' {1}
-'exactly twice' {2}
-'exactly 4 times' {4}
-'if followed by' -> capture (digit) if followed by (any character) -> /([0-9])(?=\\w)/
-'if not followed by' -> capture (letter) if not followed by (digit) /([a-z])(?![0-9])/
-'letter' [a-z]
-'letter from g to m' [g-m]
-'literally "a string to match"' -> a string to match
-'must end' $
-'multi line' /yourRegex/m
-'never or more' *
-'new line' \\n
-'no character' \\W
-'no whitespace' \\S
-'number from' -> number from 4 to 6 -> [4-6]
-'once' {1}
-'one of "defg1234"' [defg1234]
-'optional' ?
-'once or more' +
-'raw "[a-zA-Z]"' -> [a-zA-Z]
-'tab' \\t
-'twice',
-'starts with' ^
-'until' -> capture (anything once or more) until "." -> (.+?)(?:\.)
-'uppercase letter' [A-Z]
-'uppercase letter from M to P' [M-P]
-'whitespace' \\s
-
+`Translating RegEx:
 (a) capture (a)
 
 [abc] one of "abc"
@@ -66,7 +27,7 @@ $ must end
 
 {8,} at least 8 times
 
-Character classes: 
+Character classes:
 \\d digit
 
 \\w any character
@@ -83,7 +44,7 @@ Character classes:
 
 \\n new line
 
-\\ a backslash is for characters that usually have special handling, you can escape that character and it will be treated literally. For example \\d would match a digit, while \\\d will match a string that has a backslash followed by the letter d.
+\\ use a backslash for characters that usually have special handling. You can escape that character and it will be treated literally. For example \\d will match a digit, while \\\\d will match a string that has a backslash followed by the letter d.
 
 Flags:
 /yourRegex/m multi line
@@ -91,7 +52,4 @@ Flags:
 /myRegex/i 'case insensitive'
 
 Currently unsupported characters \\1 referencing a capturing group \\v vertical tab [\\b] backspace \\0 matches NUL \\cX matches ctrl-X \\uDDDD matches a given unicode character \\f matches a form feed
-'any character',
-
-
 `;
