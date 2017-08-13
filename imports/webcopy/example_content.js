6875e14317d4512f3a7b967c4e6221ced0de6246
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-08-13 11:41:39 -0700

    Enhance help text and placeholder

--- imports/webcopy/example_content.js
+++ imports/webcopy/example_content.js
@@ -17,7 +17,7 @@ Guide to Special Characters in RegEx: . matches any single character, except lin
 
 \\w matches any alphanumeric character, including underscores. Works the same as [A-Za-z0-9_]
 
-\\W  matches any character that is not a word character. Same as [^A-Za-z0-9_]
+\\W matches any character that is not a word character. Same as [^A-Za-z0-9_]
 
 \\s matches a single whitespace character.
 
@@ -27,8 +27,6 @@ Guide to Special Characters in RegEx: . matches any single character, except lin
 
 \\r matches a carriage return.
 
-\\n matches a new line or linefeed.
-
 \\ a backslash is for characters that usually have special handling, you can escape that character and it will be treated literally. For example \\d would match a digit, while \\\d will match a string that has a backslash followed by the letter d.
 
 x|y the pipe is an or operator in RegEx, it will match either x or y
@@ -37,11 +35,16 @@ x|y the pipe is an or operator in RegEx, it will match either x or y
 
 $ is the complement of the caret and indicates the string to be matched must end.
 
+Character support we are building soon:
+\\n matches a new line or linefeed.
+
 \\b matches a word boundary. It is most often used to insert characters before or after words. It has no length, since it is a concept rather than a character.
 
 \\B matches a non-word boundary.
 
+\\v vertical tab
+
 (x) surrounding part of your query in quotes creates a capturing group. It matches x and remembers the match.
 
-Currently unsupported characters \\1 referencing a capturing group \\v vertical tab [\\b] backspace \\0 matches NUL \\cX matches ctrl-X \\uDDDD matches a given unicode character \\f matches a form feed
+Currently unsupported characters \\1 referencing a capturing group [\\b] backspace \\0 matches NUL \\cX matches ctrl-X \\uDDDD matches a given unicode character \\f matches a form feed
 `;
