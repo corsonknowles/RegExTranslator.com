c7c9bd1b2551a174eda01124aff12e73cfd436f3
adam <amjacobson0@gmail.com>
2017-08-13 21:55:11 -0700

    Cleanup of help text and glossary

--- imports/components/translator/srl_input.jsx
+++ imports/components/translator/srl_input.jsx
@@ -81,6 +81,98 @@ export default connect(mapStateToProps, mapDispatchToProps)(
         }
       }
 
+      const helpTextContent = [
+        ["(a)", "capture (a)"],
+        ["(?abc)", "literally abc"],
+        ["[abc]", "one of \"abc\""],
+        ["[^abc]", "raw [^abc]"],
+        ["[a-z]", "letter"],
+        ["[A-Z]", "uppercase letter"],
+        ["plain text string", 'literally "plain text string"'],
+        ["^", "begin with"],
+        ["?", "optional"],
+        [null, null],
+        ["Quantifiers", null],
+        ["+", "one or more"],
+        ["*", "never or more"],
+        ["$", "must end"],
+        [".", "anything"],
+        ["{1}", "once"],
+        ["{2}", "twice"],
+        ["{3}", "exactly 3 times"],
+        ["{3,}", "at least 3 times"],
+        ["{3,6}", "between 3 to 6 times"],
+        [null, null],
+        ["Character classes", null],
+        ["\\d", "digit"],
+        ["\\w", "any character"],
+        ["\\W", "no character"],
+        ["\\s", "whitespace"],
+        ["\\S", "no whitespace"],
+        ["\\t", "tab"],
+        ["\\n", "new line"],
+        ["\\ Use a backslash for characters that usually have special handling. You can escape that character and it will be treated literally. For example \d will match a digit, while \\d will match a string that has a backslash followed by the letter d.", null],
+        [null, null],
+        ["Flags", null],
+        ["/regex/m", "multi line"],
+        ["/regex/i", "case insensitive"],
+        [null, null],
+        ["Coming Soon...", null],
+        ["\\r", "carriage return"],
+        ["\\v", "vertical tab"],
+        ["\\D", "no digit"],
+        ["[^abc]", "none of abc"],
+        ["\\b", "word character"],
+        ["\\b", "no word character"],
+        ["(?:a)(?=(?:X))", "literally a if followed by X"],
+        ["(?:b)(?!(?:Y))", "literally a if not followed by Y"],
+        [null, null],
+        ["Not supported", null],
+        ["\\1", "Referencing a captured group"],
+        ["\\b", "backspace"],
+        ["\\0", "Null character"],
+        ["\\cX", "crtl-X"],
+        ["\\uDDDD", "unicode character DDDD"],
+        ["\\f", "form feed"],
+      ];
+
+      const glossaryContent = [
+        ["any character", "\\w"],
+        ["any of (a, b, c)", "(a|b|c)"],
+        ["anything", "."],
+        ["at least 3 times", "{3,}"],
+        ["backslash", "\\"],
+        ["between 3 and 6 times", "{3,6}"],
+        ["capture a", "(a)"],
+        ["case insensitive", "/regex/i"],
+        ["digit", "\\d"],
+        ["digit from 3 to 5", "[3-5]"],
+        ["either of (x, y, z)", "(x|y|z)"],
+        ["exactly 3", "{3}"],
+        ["exactly 4 times", "{4}"],
+        ["letter", "[a-z]"],
+        ["letter from g to m", "[g-m]"],
+        ["literally \"stuff\"", "(?:stuff)"],
+        ["multi line", "/regex/m"],
+        ["must end", "$"],
+        ["never or more", "*"],
+        ["new line", "\\n"],
+        ["no character", "\\W"],
+        ["no whitespace", "\\S"],
+        ["number from 3 to 6", "[3-6]"],
+        ["once", "{1}"],
+        ["once or more", "+"],
+        ["one of \"defg123\"", "[defg123]"],
+        ["optional", "?"],
+        ["raw [a-zA-Z]", "[a-zA-Z]"],
+        ["starts with", "^"],
+        ["tab", "\\t"],
+        ["twice", "{2}"],
+        ["uppercase letter", "[A-Z]"],
+        ["uppercase letter from D to Y", "[D-Y]"],
+        ["whitespace", "\\s"],
+      ];
+
     return (
         <div className="translator-input-section">
           <header>
@@ -96,77 +188,18 @@ export default connect(mapStateToProps, mapDispatchToProps)(
             className={classes.join(' ')} />
 
           <header>
-            <h2>Help Text</h2>
+            <h2>Syntax: Simple Regex Language vs Regex</h2>
           </header>
 
           <div className="help-text">
-            (a) capture (a)<br />
-
-            (?abc) literally abc<br />
-
-            [abc] one of "abc"<br />
-
-            [^abc] raw [^abc]<br />
-
-            [a-z] letter<br />
-
-            [A-Z] uppercase letter<br />
-
-            "plain text string" -> literally "plain text string"<br />
-
-            ^ starts with<br />
-
-            ? optional<br />
-
-            $ must end<br />
-
-            . anything<br />
-
-            + once or more<br />
-
-            * never or more<br />
-
-            &#123;8,&#125; at least 8 times<br />
-
-            &#123;5&#125; exactly 5 times<br />
-
-            &#123;7-9&#125; between 7 and 9 times<br />
-
-            &#123;1&#125; once<br />
-
-            &#123;2&#125; twice<br />
-
-            Lookaheads:<br />
-            (?:a)(?=(?:9)) literally a if followed by 9<br />
-
-            ((?:b))(?!(?:8)) capture b if not followed by 8<br />
-
-            Character classes:<br />
-            \d digit<br />
-
-            \w any character<br />
-
-            \W no character<br />
-
-            \s whitespace<br />
-
-            \S no whitespace<br />
-
-            \t tab<br />
-
-            \n new line<br />
-
-            \ use a backslash for characters that usually have special handling. You can escape that character and it will be treated literally. For example \d will match a digit, while \\d will match a string that has a backslash followed by the letter d.<br />
-            <br />
-            Flags:<br />
-            /yourRegex/m multi line<br />
-
-            /myRegex/i case insensitive<br />
-            <br />
-            Coming soon:<br />\r carriage return  \v vertical tab  \D no digit [^abc] none of "abc" \b word \B no word<br />
-            <br />
-            Currently unsupported characters: <br />\1 referencing a capturing group [\b] backspace \0 NUL \cX ctrl-X \uDDDD for a given unicode character \f form feed<br />
-            <br />
+            {helpTextContent.map(pair => {
+              return (
+                <div className="row">
+                  {pair[0]? (<div className="column">{pair[0]}</div>) : (<div className="column">&nbsp;</div>) }
+                  {pair[1]? (<div className="column">{pair[1]}</div>) : (<div className="column">&nbsp;</div>) }
+                </div>
+              );
+            })}
           </div>
 
           <header>
@@ -174,49 +207,14 @@ export default connect(mapStateToProps, mapDispatchToProps)(
           </header>
 
           <div className="help-text">
-
-            Glossary:<br />
-            any character \w<br />
-            any of (a, b, c) (a|b|c)<br />
-            anything .<br />
-            at least 8 times &#123;8,&#125;<br />
-            backslash \<br />
-            between 3 and 7 times &#123;3,7&#125;<br />
-            capture a (a)<br />
-            case insensitive /myRegex/i<br />
-            digit \d<br />
-            digit from 3 to 5 [3-5]<br />
-            either of (x, y, z) (x|y|z)<br />
-            exactly 3 &#123;3&#125;<br />
-            exactly 4 times &#123;4&#125;<br />
-            if followed by -> capture (digit) if followed by (any character) -> /([0-9])(?=\w)/<br />
-            if not followed by -> capture (letter) if not followed by (digit) /([a-z])(?![0-9])/<br />
-            letter [a-z]<br />
-            letter from g to m [g-m]<br />
-            literally "a string to match" -> (?:a string to match)<br />
-            multi line /yourRegex/m<br />
-            must end $<br />
-            never or more *<br />
-            new line \n<br />
-            no character \W<br />
-            no whitespace \S<br />
-            number from -> number from 4 to 6 -> [4-6]<br />
-            once &#123;1&#125;<br />
-            one of "defg1234" [defg1234]<br />
-            optional ?<br />
-            once or more +<br />
-            raw "[a-zA-Z]" -> [a-zA-Z]<br />
-            starts with ^<br />
-            tab \t<br />
-            twice &#123;2&#125;<br />
-            until -> capture (anything once or more) until "." -> (.+?)(?:\.)<br />
-            uppercase letter [A-Z]<br />
-            uppercase letter from M to P [M-P]<br />
-            whitespace \s<br />
-
-            Coming soon: backslash, none of<br />
-            <br />
-
+            {glossaryContent.map(pair => {
+              return (
+                <div className="row">
+                  {pair[0]? (<div className="column">{pair[0]}</div>) : (<div className="column">&nbsp;</div>) }
+                  {pair[1]? (<div className="column">{pair[1]}</div>) : (<div className="column">&nbsp;</div>) }
+                </div>
+              );
+            })}
           </div>
         </div>
       );
